b0e298515a817c1badc31d375338f6d98359e21d
Djason NATHIEZ <d.nathiez@rubika-edu.com>
2022-02-09 00:04:17 +0100

    Import TMP + mise en place UI debug input et stats player

--- ZA Groupe 2/Assets/Code/Scripts/Player/PlayerManager.cs
+++ ZA Groupe 2/Assets/Code/Scripts/Player/PlayerManager.cs
@@ -10,11 +10,13 @@ public class PlayerManager : MonoBehaviour
 {
     private InputController m_inputController;
     private Rigidbody m_rb;
-
+    
+    public PlayerStateMachine playerStateMachine;
+    public enum PlayerStateMachine { IDLE, MOVE, ATTACK };
+    
     [Header("Current Statistics")] 
     public float speed;
-
-    private Vector3 moveDirection;
+    private Vector3 m_moveDirection;
     
     private void Awake()
     {
@@ -25,20 +27,30 @@ public class PlayerManager : MonoBehaviour
 
     private void Update()
     {
-        m_inputController.Player.Move.performed += context => moveDirection = new Vector3(context.ReadValue<Vector2>().x, 0, context.ReadValue<Vector2>().y);
-        m_inputController.Player.Move.canceled += context => moveDirection = Vector3.zero;
+        m_inputController.Player.Move.performed += context => m_moveDirection = new Vector3(context.ReadValue<Vector2>().x, 0, context.ReadValue<Vector2>().y);
+        m_inputController.Player.Move.canceled += context => m_moveDirection = Vector3.zero;
         
         Move();
     }
 
     private void Move()
     {
-        m_rb.velocity = moveDirection * speed;
+        //move to the direction of the input by movement speed
+        m_rb.velocity = m_moveDirection * speed;
 
-        if (moveDirection != Vector3.zero)
+        if (m_moveDirection != Vector3.zero)
         {
-            Quaternion lookRotation = Quaternion.LookRotation(moveDirection); 
+            //change the look direction to the last move direction
+            Quaternion lookRotation = Quaternion.LookRotation(m_moveDirection); 
             m_rb.MoveRotation(lookRotation);
+            
+            //switch the state of player to MOVE, which means the player is moving
+            playerStateMachine = PlayerStateMachine.MOVE;
+        }
+        else
+        {
+            //in case that the moveDirection Vector is equal to zero, this mean the player isn't moving so he back to the idle state
+            playerStateMachine = PlayerStateMachine.IDLE;
         }
     }
     
---
+++ ZA Groupe 2/Assets/_Developper/DEV_DJN/Scripts/PlayerDebug.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerDebug : MonoBehaviour
+{
+   private PlayerManager m_playerManager;
+
+   [Header("Player Stats Debug Informations")]
+   public TextMeshProUGUI speedTxt;
+
+   public TextMeshProUGUI healthTxt;
+
+   [Header("Input Callback Debug Informations")]
+   public TextMeshProUGUI leftStickXTxt;
+
+   public TextMeshProUGUI rightStickTxt;
+
+   private void Awake()
+   {
+      m_playerManager = FindObjectOfType<PlayerManager>();
+   }
+
+}
