af74ecd6af004b455104b165197e161ddbbb5d96
Djason NATHIEZ <d.nathiez@rubika-edu.com>
2022-03-02 04:32:30 +0100

    Able to switch area form circle to square

--- ZA Groupe 2/Assets/Code/Scripts/Enemy/SpawnArea.cs
+++ ZA Groupe 2/Assets/Code/Scripts/Enemy/SpawnArea.cs
@@ -7,17 +7,38 @@ using Random = System.Random;
 
 public class SpawnArea : MonoBehaviour
 {
-    public float rangeArea;
-    private Collider[] area;
+    [Header("Setup")]
+    public AreaType areaType;
+    public enum AreaType{CIRCLE, SQUARE}
+    
     public float minX;
     public float maxX;
     public float minZ;
     public float maxZ;
+    
+    private Collider[] area;
+    
+    //for a circle
+    [Header("Circle")]
+    [Range(0, 50)] public float rangeArea;
+    
+     
 
+    //for a square
+    [Header("Square")]
+    [Range(0, 50)] public float xAxisRange;
+    [Range(0, 50)] public float zAxisRange;
+    
     [Header("EnemySpawner")] 
     public int spawnNumber;
     public GameObject[] spawnableList;
 
+    private Vector3 spawnPoint;
+
+    private void OnValidate()
+    {
+        SetArea();
+    }
 
     void Start()
     {
@@ -27,12 +48,31 @@ public class SpawnArea : MonoBehaviour
 
     void SetArea()
     {
-        area = Physics.OverlapSphere(transform.position, rangeArea);
 
-        minX = transform.position.x - rangeArea;
-        minZ = transform.position.z - rangeArea;
-        maxX = transform.position.x + rangeArea;
-        maxZ = transform.position.z + rangeArea;
+        switch (areaType)
+        {
+            case AreaType.CIRCLE:
+                area = Physics.OverlapSphere(transform.position, rangeArea);
+                
+                minX = transform.position.x - rangeArea;
+                minZ = transform.position.z - rangeArea;
+                maxX = transform.position.x + rangeArea;
+                maxZ = transform.position.z + rangeArea;
+                break;
+            
+            case AreaType.SQUARE:
+                Vector3 square = new Vector3(xAxisRange, 0, zAxisRange);
+                area = Physics.OverlapBox(transform.position, square);
+
+                minX = transform.position.x - xAxisRange;
+                minZ = transform.position.z - zAxisRange;
+                maxX = transform.position.x + xAxisRange;
+                maxZ = transform.position.z + zAxisRange;
+                break;
+            
+        }
+
+       
         
     }
 
@@ -40,7 +80,7 @@ public class SpawnArea : MonoBehaviour
     {
         for (int i = 0; i < spawnNumber; i++)
         {
-            Vector3 spawnPoint = new Vector3(UnityEngine.Random.Range(minX, maxX), 0, UnityEngine.Random.Range(minZ, maxZ));
+            spawnPoint = new Vector3(UnityEngine.Random.Range(minX, maxX), 0, UnityEngine.Random.Range(minZ, maxZ));
 
             GameObject newEnemy = Instantiate(spawnableList[UnityEngine.Random.Range(0, spawnableList.Length)], spawnPoint, Quaternion.identity);
         }
@@ -48,6 +88,16 @@ public class SpawnArea : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawWireSphere(transform.position, rangeArea);
+        switch (areaType)
+        {
+            case AreaType.CIRCLE:
+                Gizmos.DrawWireSphere(transform.position, rangeArea);
+                break;
+            
+            case AreaType.SQUARE:
+                Vector3 drawCube = new Vector3(maxX - minX, 0, maxZ - minZ);
+                Gizmos.DrawWireCube(transform.position, drawCube);
+                break;
+        }
     }
 }
