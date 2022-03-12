c62d99c0db6dff0ea1d50a49bd80f6dca0c47404
Mathis Jibert <m.jibert@rubika-edu.com>
2022-03-12 18:09:52 +0100

    Systeme de Dialogues

--- ZA Groupe 2/Assets/Code/Scripts/Camera/CameraController.cs
+++ ZA Groupe 2/Assets/Code/Scripts/Camera/CameraController.cs
@@ -6,14 +6,26 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     private GameObject m_player;
+    private Camera m_camera;
+    public bool playerFocused;
+    public Transform m_cameraPos;
+    public float m_cameraZoom;
 
     private void Awake()
     {
          m_player = GameObject.FindGameObjectWithTag("Player");
+         m_camera = GetComponent<Camera>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        transform.position = m_player.transform.position;
+        transform.position = Vector3.Lerp(transform.position,m_cameraPos.position, 0.05f);
+        transform.rotation = Quaternion.Lerp(transform.rotation,m_cameraPos.rotation,0.05f);
+        m_camera.orthographicSize = Mathf.Lerp(m_camera.orthographicSize, m_cameraZoom, 0.05f);
+
+        if (playerFocused)
+        {
+            m_cameraPos.position = m_player.transform.position;   
+        }
     }
 }
