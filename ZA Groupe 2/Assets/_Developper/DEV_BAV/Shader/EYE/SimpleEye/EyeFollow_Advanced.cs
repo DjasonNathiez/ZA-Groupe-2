a4a7274cdfc2e87d21c3c9a763669a20a9aa5af1
Baptiste VALLE <baptiste.valle98@gmail.com>
2022-04-28 13:26:39 +0200

    Lion_VFX_V1

--- ZA Groupe 2/Assets/_Developper/DEV_BAV/Shader/EYE/SimpleEye/EyeFollow_Advanced.cs
+++ ZA Groupe 2/Assets/_Developper/DEV_BAV/Shader/EYE/SimpleEye/EyeFollow_Advanced.cs
@@ -8,11 +8,13 @@ using Vector4 = UnityEngine.Vector4;
 public class EyeFollow_Advanced : MonoBehaviour
 {
     //public List<Material> materialZChange;
-
+    [SerializeField] private float adjustFollow;
+    [SerializeField] private Transform objectToFollow;
     [SerializeField]private List<GameObject> objectOnXWall;
     //private List<GameObject> objectOnZWall;
     
     public List<Material> matObjectX;
+    public List<Vector3> objectTransformX;
     public Vector3 roomSize;
     private Vector3 positionObjectToFollow;
     
@@ -20,9 +22,10 @@ public class EyeFollow_Advanced : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        foreach (GameObject objectX in objectOnXWall)
+        foreach (GameObject obj in objectOnXWall)
         {
-            matObjectX.Add(objectX.GetComponent<MeshRenderer>().sharedMaterial);
+            objectTransformX.Add(obj.transform.localPosition);
+            matObjectX.Add(obj.GetComponent<MeshRenderer>().material);
         }
     }
 
@@ -36,16 +39,26 @@ public class EyeFollow_Advanced : MonoBehaviour
     {
         float eyeMaxOffset = 0.5f;
         
+        /*
         for (int m = 0; m < objectOnXWall.Count; m++)
         {
             for (int i = 0; i < matObjectX.Count; i++)
             {
                 positionObjectToFollow.x *= objectOnXWall[i].transform.position.x;
                 positionObjectToFollow.x = Mathf.Clamp(positionObjectToFollow.x,-eyeMaxOffset, eyeMaxOffset);
-                matObjectX[m].SetVector("_EyePosition", new Vector4(-positionObjectToFollow.x , positionObjectToFollow.y , 0, 1));
+                
             }
             Debug.Log(positionObjectToFollow.x);
 
+        }
+        */
+
+        for (int i = 0; i < matObjectX.Count; i++)
+        {
+            matObjectX[i].SetVector("_EyePosition", new Vector4(Mathf.Clamp((objectToFollow.transform.position.z - objectTransformX[i].z*adjustFollow),
+                -0.5f * adjustFollow, 
+                0.5f* adjustFollow), 0, 0, 1));
+        
         }
     }
 
