56ce08bba6b195c362430bb5f40cc05a23ec7457
Baptiste Valle <baptiste.valle98@gmail.com>
2022-05-17 01:10:28 +0200

    Commit

--- ZA Groupe 2/Assets/_Developper/DEV_BAV/Script/Transition.cs
+++ ZA Groupe 2/Assets/_Developper/DEV_BAV/Script/Transition.cs
@@ -3,17 +3,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteAlways]
 public class Transition : MonoBehaviour
 {
     [SerializeField] float radius;
-
-    [SerializeField] private Transform playerPos;
+    
     [SerializeField] private Transform etagePos;
+    [SerializeField] private float heightPlayer;
+    [SerializeField] private float _duration;
+    [SerializeField]
+    
+    //Private float
+    private float _speed;
+
+    private void Start()
+    {
+        radius= 0f;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        Shader.SetGlobalVector("_PlayerPosition", transform.position);
+        Vector3 playerPosition = PlayerManager.instance.transform.position;
+        Shader.SetGlobalVector("_PlayerPosition", playerPosition);
+        heightPlayer =  (etagePos.localPosition.y + 1) - playerPosition.y;
+        //Debug.Log(heightPlayer);
+        if (heightPlayer != 0)
+        {
+            if (heightPlayer >= 0)
+            {
+                radius = Mathf.Lerp(0, 10, heightPlayer.Remap(4, 2, 0, 1));
+            }
+            Shader.SetGlobalFloat("_TransitionRadius", radius);
+        }
+    }
+
+    private void OnDisable()
+    {
+        radius = 200f;
         Shader.SetGlobalFloat("_TransitionRadius", radius);
     }
 }
