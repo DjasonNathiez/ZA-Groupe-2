cc1cd911fe7c6a86b08d93348eb09185cf148294
Philémon MOREL <p.morel@rubika-edu.com>
2022-05-18 12:00:56 +0200

    Création Scène test Objet + ValueTrack afin de tirer les objets

---
+++ ZA Groupe 2/Assets/_Developper/DEV_PHM/ValueTrack.cs
@@ -0,0 +1,17 @@
+using System;using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WeightClass
+{
+    NULL,
+    LIGHT,
+    MEDIUM,
+    HEAVY,
+}
+public class ValueTrack : MonoBehaviour
+{
+    public WeightClass weightClass;
+}
+
+
