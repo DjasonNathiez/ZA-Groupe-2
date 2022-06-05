109614695b79fd39828003d4a2b52a086e518006
Philémon MOREL <p.morel@rubika-edu.com>
2022-06-05 18:27:54 +0200

    Fix du Manoir

--- ZA Groupe 2/Assets/Code/Scripts/Puzzle/ManoirLabyrintheDarknest.cs
+++ ZA Groupe 2/Assets/Code/Scripts/Puzzle/ManoirLabyrintheDarknest.cs
@@ -7,11 +7,11 @@ public class ManoirLabyrintheDarknest : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player")) other.gameObject.GetComponent<Transition_Fog>().isPuzzle = true;
+        if (other.CompareTag("Player")) GameManager.instance.GetComponentInChildren<Transition_Fog>().isPuzzle = true;
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player")) other.gameObject.GetComponent<Transition_Fog>().isPuzzle = false;
+        if (other.CompareTag("Player")) GameManager.instance.GetComponentInChildren<Transition_Fog>().isPuzzle = false;
     }
 }
