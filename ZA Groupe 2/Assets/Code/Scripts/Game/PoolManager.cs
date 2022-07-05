aae6996210b5d371a49b7ecf7bc43ae21dc44072
Arcaane <cordaurelien@gmail.com>
2022-07-05 16:23:53 +0200

    Update PoolManager.cs

--- ZA Groupe 2/Assets/Code/Scripts/Game/PoolManager.cs
+++ ZA Groupe 2/Assets/Code/Scripts/Game/PoolManager.cs
@@ -17,37 +17,7 @@ public class PoolManager : MonoBehaviour
     
     public enum Object
     {
-        // Bear Vfx
-        VFX_BearHurt = 0,
-        VFX_BearDeathExplosion, 
-        VFX_BearAttack,
-        VFX_BearHitable,
-
-        // Lion Vfx
-        VFX_LionDeathExplosion, 
-        VFX_Lion_Fall, 
-        VFX_Lion_Death, 
-        VFX_Lion_StandUp, 
-        VFX_Lion_Hurt, 
-        VFX_Lion_Attack,
-        
-        // Rabbit
-        VFX_Rabbit_Chase_Surprise,
-        VFX_Rabbit_Attack, 
-        VFX_Rabbit_Damage, 
-        VFX_Rabbit_Death, 
-        VFX_Rabbit_Chase_Begin, 
-        VFX_Rabbit_Chase_Continue, 
-        VFX_Rabbit_Chase_End, 
-        VFX_Rabbit_Manga_Angry, 
-        
-        // Kent
-        VFX_KentHurt, 
-        VFX_KentAttack, 
-        VFX_KentRoll, 
-        VFX_Rope_Thow, 
-        VFX_PuzzleReussite
-        
+        Obj1, Obj2, Obj3
     }
     
     [System.Serializable]
@@ -58,7 +28,7 @@ public class PoolManager : MonoBehaviour
     }
     
 
-    [SerializeField] private Pool[] poolArray = new Pool[22];
+    [SerializeField] private Pool[] poolArray = new Pool[3];
     private Dictionary<Object, Queue<GameObject>> queueDictionary = new Dictionary<Object, Queue<GameObject>>(); // Store Queue of Object
     private Dictionary<Object, GameObject> prefabDictionary = new Dictionary<Object, GameObject>(); // Store prefabs independently of Queue
 
