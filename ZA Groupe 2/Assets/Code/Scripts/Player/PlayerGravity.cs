2ca321cf7268ca85b537724de6d6c08298fdb5ef
Djason NATHIEZ <d.nathiez@rubika-edu.com>
2022-07-06 17:37:06 +0200

    bouncing but not smooth

--- ZA Groupe 2/Assets/Code/Scripts/Player/PlayerGravity.cs
+++ ZA Groupe 2/Assets/Code/Scripts/Player/PlayerGravity.cs
@@ -8,6 +8,8 @@ public class PlayerGravity : MonoBehaviour
     public CapsuleCollider capsuleCollider;
     public Rigidbody rb;
 
+    public bool isBouncing;
+
     [Header("Ground Check")]
     public bool playerIsGrounded = true;
     [Range(0.0f, 1.0f)] public float groundCheckRadiusMultiplier = 0.9f;
@@ -22,10 +24,17 @@ public class PlayerGravity : MonoBehaviour
     public float gravityFallCurrent = -100.0f;
     public float gravityFallMin = -100.0f;
     public float gravityFallMax = -500.0f;
-    [Range(-5.0f, -35.0f)] public float gravityFallIncrementAmount = -20.0f;
+    /*[Range(-5.0f, -35.0f)]*/ public float gravityFallIncrementAmount = -20.0f;
     public float gravityFallIncrementTime = 0.05f;
     public float playerFallTimer = 0.0f;
     public float gravity = 0.0f;
+
+    [Header("Bouncing")] 
+    public float bounceTime;
+    private float bounceInTimer;
+    private bool reachTheSky;
+    
+    private Vector3 bounceDir;
 
     private void Awake()
     {
@@ -34,20 +43,66 @@ public class PlayerGravity : MonoBehaviour
         stepRayUpper.transform.position = position;
     }
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(rb.position, (capsuleCollider.bounds.extents.y - (capsuleCollider.radius * groundCheckRadiusMultiplier)) + groundCheckDistance);
+    }
+
     private void FixedUpdate()
     {
         playerIsGrounded = PlayerGroundCheck();
         rb.velocity = new Vector3(rb.velocity.x, PlayerGravityMethod() ,rb.velocity.z);
+
+        Debug.Log(DistanceToGround());
+        
+        if (isBouncing)
+        {
+            if(DistanceToGround() < 4 && reachTheSky == false)
+            {
+                rb.AddForce(bounceDir, ForceMode.Impulse);
+                bounceInTimer -= Time.deltaTime;
+                gravity = 0;
+
+                if (DistanceToGround() >= 3.99)
+                {
+                    reachTheSky = true;
+                }
+                
+            }
+            
+            if (reachTheSky)
+            {
+                gravity += -5 * Time.deltaTime;
+                
+                if (DistanceToGround() <= 1)
+                {
+                    reachTheSky = false;
+                    isBouncing = false;
+                }
+            }
+            
+        }
         
         StepClimb();
     }
 
     private bool PlayerGroundCheck()
     {
-        float sphereCastRadius = capsuleCollider.radius * groundCheckRadiusMultiplier;
-        float sphereCastTravelDistance = capsuleCollider.bounds.extents.y - sphereCastRadius + groundCheckDistance;
-        return Physics.SphereCast(rb.position, sphereCastRadius, Vector3.down, out _groundCheckHit,
-            sphereCastTravelDistance);
+        return DistanceToGround() < 0.8;
+    }
+
+    private float DistanceToGround()
+    {
+        RaycastHit groundHit;
+
+        if (Physics.Raycast(rb.position, Vector3.down, out groundHit, 1000))
+        {
+            float distanceToGround = Vector3.Distance(transform.position, groundHit.point);
+            return distanceToGround;
+        }
+        Debug.DrawLine(rb.position, Vector3.down);
+
+        return 0;
     }
 
     private float PlayerGravityMethod()
@@ -59,15 +114,18 @@ public class PlayerGravity : MonoBehaviour
         }
         else
         {
-            playerFallTimer -= Time.fixedDeltaTime;
-            if (playerFallTimer < 0.0f)
+            if (!isBouncing)
             {
-                if (gravityFallCurrent > gravityFallMax)
+                playerFallTimer -= Time.fixedDeltaTime;
+                if (playerFallTimer < 0.0f)
                 {
-                    gravityFallCurrent += gravityFallIncrementAmount;
+                    if (gravityFallCurrent < gravityFallMax)
+                    {
+                        gravityFallCurrent += gravityFallIncrementAmount;
+                    }
+                    playerFallTimer = gravityFallIncrementTime;
+                    gravity = gravityFallCurrent;
                 }
-                playerFallTimer = gravityFallIncrementTime;
-                gravity = gravityFallCurrent;
             }
         }
 
@@ -84,6 +142,14 @@ public class PlayerGravity : MonoBehaviour
                 out hitUpper, 0.2f))
                 rb.position -= new Vector3(0f, -stepSmooth, 0f);
         }
+    }
+
+    public void BounceEffect(float bHeight, float bForce)
+    {
+        gravityFallCurrent = -50;
+        bounceInTimer = bounceTime;
+        isBouncing = true;
+        bounceDir = new Vector3(0, bHeight * bForce, 0);
     }
 
 }
