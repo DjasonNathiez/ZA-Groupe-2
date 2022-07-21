f504c624324741e7cd9740d5d1b1c2ea9283a039
Philémon <p.morel@rubika-edu.com>
2022-07-21 17:46:09 +0200

    Début cr"ation salle

---
+++ ZA Groupe 2/Assets/Code/Scripts/Puzzle/MovingPressurePlate.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPressurePlate : MonoBehaviour
+{
+    [SerializeField] private Transform[] limitRailRoad;
+    [SerializeField] private GameObject rightMovingButton;
+    [SerializeField] private GameObject leftMovingButton;
+    
+    
+}
