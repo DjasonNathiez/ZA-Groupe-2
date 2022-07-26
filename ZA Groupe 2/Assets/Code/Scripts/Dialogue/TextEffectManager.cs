00c6dc16770148c1b9c21a32c1daa00bb4edd6d8
Aurélien <cordaurelien@gmail.com>
2022-07-26 15:25:49 +0200

    UI NAV + Resolutions / Fullscreen

--- ZA Groupe 2/Assets/Code/Scripts/Dialogue/TextEffectManager.cs
+++ ZA Groupe 2/Assets/Code/Scripts/Dialogue/TextEffectManager.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
