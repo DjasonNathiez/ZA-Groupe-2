be532eedf1cf54a543f9cb821f7bbfc40b244a58
Philémon <p.morel@rubika-edu.com>
2022-07-27 13:08:46 +0200

    Puzzle du Manoir Fix

--- ZA Groupe 2/Assets/Code/Scripts/Puzzle/DestroyComponent.cs
+++ ZA Groupe 2/Assets/Code/Scripts/Puzzle/DestroyComponent.cs
@@ -8,12 +8,18 @@ public class DestroyComponent : MonoBehaviour
     public PnjDialoguesManager PnjDialoguesManager;
     public closeDoorCollision CloseDoorCollision;
     public Vector3 teleportPoint;
+    public GameObject plateforme;
+    public Vector3 position;
     
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             other.transform.position = teleportPoint;
+            if (plateforme != null)
+            {
+                plateforme.transform.position = position;
+            }
         }
     }
 }
